17e607425c5b5560f9dc6131d547afe037398992
Ivan Arvizu <ivan@simbo.mx>
2017-06-28 02:41:24 -0700

    Para seguirle después

---
+++ Lab11/Lab11/MainActivity.cs
@@ -0,0 +1,74 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace Lab11
+{
+    [Activity(Label = "Lab11", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        private int Counter = 0;
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            Android.Util.Log.Debug("Lab11Log", "Activity A - Oncreate");
+
+            // Set our view from the "main" layout resource
+            SetContentView (Resource.Layout.Main);
+
+            FindViewById<Button>(Resource.Id.StartActivity).Click += (sender, e) =>
+            {
+                var ActivityIntent = new Android.Content.Intent(this, typeof(SecondActivity));
+                StartActivity(ActivityIntent);
+            };
+
+            var ClickCounter = FindViewById<Button>(Resource.Id.ClicksCounter);
+            ClickCounter.Text = Resources.GetString(Resource.String.ClicksCounter_Text, Counter);
+
+            ClickCounter.Click += (sender, e) =>
+            {
+                Counter++;
+                ClickCounter.Text = Resources.GetString(Resource.String.ClicksCounter_Text, Counter);
+            };
+
+        }
+        //--------------------------------------------------------------------------------------------
+        protected override void OnStart()
+        {
+            Android.Util.Log.Debug("Lab11Log", "Activity A - OnStart");
+            base.OnStart();
+        }
+        //--------------------------------------------------------------------------------------------
+        protected override void OnResume()
+        {
+            Android.Util.Log.Debug("Lab11Log", "Activity A - OnResume");
+            base.OnResume();
+        }
+        //--------------------------------------------------------------------------------------------
+        protected override void OnPause()
+        {
+            Android.Util.Log.Debug("Lab11Log", "Activity A - OnPause");
+            base.OnPause();
+        }
+        //--------------------------------------------------------------------------------------------
+        protected override void OnStop()
+        {
+            Android.Util.Log.Debug("Lab11Log", "Activity A - OnStop");
+            base.OnStop();
+        }
+        //--------------------------------------------------------------------------------------------
+        protected override void OnDestroy()
+        {
+            Android.Util.Log.Debug("Lab11Log", "Activity A - OnDestroy");
+            base.OnDestroy();
+        }
+        //--------------------------------------------------------------------------------------------
+        protected override void OnRestart()
+        {
+            Android.Util.Log.Debug("Lab11Log", "Activity A - OnRestart");
+            base.OnRestart();
+        }
+    }
+}
+
---
+++ Lab11/Lab11/SecondActivity.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Lab11
+{
+    [Activity(Label = "SecondActivity")]
+    public class SecondActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+        }
+        protected override void OnStart()
+        {
+            Android.Util.Log.Debug("Lab11Log", "Activity b - OnStart");
+            base.OnStart();
+        }
+        //--------------------------------------------------------------------------------------------
+        protected override void OnResume()
+        {
+            Android.Util.Log.Debug("Lab11Log", "Activity b - OnResume");
+            base.OnResume();
+        }
+        //--------------------------------------------------------------------------------------------
+        protected override void OnPause()
+        {
+            Android.Util.Log.Debug("Lab11Log", "Activity b - OnPause");
+            base.OnPause();
+        }
+        //--------------------------------------------------------------------------------------------
+        protected override void OnStop()
+        {
+            Android.Util.Log.Debug("Lab11Log", "Activity b - OnStop");
+            base.OnStop();
+        }
+        //--------------------------------------------------------------------------------------------
+        protected override void OnDestroy()
+        {
+            Android.Util.Log.Debug("Lab11Log", "Activity b - OnDestroy");
+            base.OnDestroy();
+        }
+        //--------------------------------------------------------------------------------------------
+        protected override void OnRestart()
+        {
+            Android.Util.Log.Debug("Lab11Log", "Activity b - OnRestart");
+            base.OnRestart();
+        }
+    }
+
+}
