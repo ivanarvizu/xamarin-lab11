0fcf2a37d81df6feb46d2dbb9fb090aacd76c0bf
Iván Arvizu <ivanarvizu@outlook.com>
2017-06-29 14:16:03 -0700

    Antes de la validacion

--- Lab11/Lab11/MainActivity.cs
+++ Lab11/Lab11/MainActivity.cs
@@ -33,6 +33,15 @@ namespace Lab11
                 StartActivity(ActivityIntent);
             };
 
+            Data = (Complex)this.FragmentManager.FindFragmentByTag("Data");
+            if(Data == null)
+            {
+                Data = new Complex();
+                var FragmentTransaction = this.FragmentManager.BeginTransaction();
+                FragmentTransaction.Add(Data, "Data");
+                FragmentTransaction.Commit();
+            }
+
             if(bundle != null)
             {
                 Counter = bundle.GetInt("CounterValue", 0);
@@ -41,11 +50,16 @@ namespace Lab11
 
             var ClickCounter = FindViewById<Button>(Resource.Id.ClicksCounter);
             ClickCounter.Text = Resources.GetString(Resource.String.ClicksCounter_Text, Counter);
+            ClickCounter.Text += $"\n{Data.ToString()}";
 
             ClickCounter.Click += (sender, e) =>
             {
                 Counter++;
                 ClickCounter.Text = Resources.GetString(Resource.String.ClicksCounter_Text, Counter);
+
+                Data.Real++;
+                Data.Imaginary++;
+                ClickCounter.Text += $"\n{Data.ToString()}";
             };
 
         }
---
+++ Lab11/Lab11/obj/Debug/android/src/md5cb751fe4d4017fddc0a7955c8bdc0820/Complex.java
@@ -0,0 +1,56 @@
+package md5cb751fe4d4017fddc0a7955c8bdc0820;
+
+
+public class Complex
+	extends android.app.Fragment
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_toString:()Ljava/lang/String;:GetToStringHandler\n" +
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("Lab11.Complex, Lab11, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", Complex.class, __md_methods);
+	}
+
+
+	public Complex () throws java.lang.Throwable
+	{
+		super ();
+		if (getClass () == Complex.class)
+			mono.android.TypeManager.Activate ("Lab11.Complex, Lab11, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public java.lang.String toString ()
+	{
+		return n_toString ();
+	}
+
+	private native java.lang.String n_toString ();
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
